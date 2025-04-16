287abf62d7889932d826bce5483a634d5858e119
TromiUss <121058447+TromiUss@users.noreply.github.com>
2025-04-16 23:20:52 +0300

    tsx card

---
+++ src/components/NewsSnippet.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { IData_SnippetNews } from '../types/interface';
+
+const NewsComp: React.FC<IData_SnippetNews> = ({ ID, TI, AB, URL, DOM, DP, LANG, REACH, KW, AU, CNTR, CNTR_CODE, SENT, TRAFFIC, FAV, HIGHLIGHTS }) => {
+  return (
+    <section className="card">
+      <div className="card-header">
+        <div className="card-title">{TI}</div>
+
+        <div className="card-sub">
+          <div className="source">{DOM}</div>
+          <div className="location">{CNTR}</div>
+          <div className="author">{AU.join(', ')}</div>
+        </div>
+        <div className="tag-section">
+          {KW.map((tag, index) => (
+            <div key={index} className="tag">
+              {tag}
+            </div>
+          ))}
+        </div>
+
+        <div className="button-row">
+          <button className="btn" onClick={() => window.open(URL, '_blank')}>Original Source</button>
+          <button className="btn">View Duplicates</button>
+        </div>
+        <div className="duplicate">
+          <div className="card-title">{TI}</div>
+          <div className="card-sub">
+            <div className="source">{DOM}</div>
+            <div className="location">{CNTR}</div>
+            <div className="author">{AU.join(', ')}</div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default NewsComp;
